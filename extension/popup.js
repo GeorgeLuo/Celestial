37d19be55087c187c728828965ae30f639c3c23b
George Luo <gluo@Georges-MacBook-Air.local>
2023-12-29 18:55:32 -0800

    setting window size

--- extension/popup.js
+++ extension/popup.js
@@ -1,16 +1,30 @@
 // popup.js
 document.getElementById('startCapture').addEventListener('click', function() {
-  chrome.tabs.captureVisibleTab(null, {}, function(dataUrl) {
-    console.log("startCapture.");
-    let initialScreenshot = dataUrl; // Initial screenshot is taken
-    localStorage.setItem('initialScreenshot', initialScreenshot);
+  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+    let activeTab = tabs[0];
+    
+    chrome.tabs.captureVisibleTab(null, {}, function(dataUrl) {
+      console.log("startCapture.");
+      let initialScreenshot = dataUrl; // Initial screenshot is taken
+      localStorage.setItem('initialScreenshot', initialScreenshot);
 
-    // Sending a message to the background script instead of the content script.
-    chrome.runtime.sendMessage({action: "startCapture"});
+      // Sending a message to the background script with the tab ID.
+      chrome.runtime.sendMessage({
+        action: "startCapture",
+        tabId: activeTab.id
+      });
+    });
   });
 });
 
 document.getElementById('stopCapture').addEventListener('click', function() {
-  // Sending a message to the background script instead of the content script.
-  chrome.runtime.sendMessage({action: "stopCapture"});
+  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+    let activeTab = tabs[0];
+    
+    // Sending a message to the background script with the tab ID.
+    chrome.runtime.sendMessage({
+      action: "stopCapture",
+      tabId: activeTab.id
+    });
+  });
 });
