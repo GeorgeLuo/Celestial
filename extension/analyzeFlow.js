aaaa3522895af02a8d6ded4d88dcead5f881f2b1
George Luo <gluo@Georges-MacBook-Air.local>
2024-01-05 00:28:58 -0800

    human readable labels of screenshots

--- extension/analyzeFlow.js
+++ extension/analyzeFlow.js
@@ -1,8 +1,13 @@
 // Function to fetch the flow data from the localStorage
+// Function to fetch the flow data from the localStorage
 function fetchFlowData() {
     var flowData = localStorage.getItem('selectedFlowData');
     if (flowData) {
-        return JSON.parse(flowData);
+        flowData = JSON.parse(flowData);
+        flowData.screenshots = flowData.screenshots.filter(function (screenshot) {
+            return screenshot.label !== 'KEY_INPUT' || screenshot.values.endKeyInput === true;
+        });
+        return flowData;
     }
     return null;
 }
@@ -21,13 +26,33 @@ function displayImagesWithLabels(flow) {
         var labelInput = document.createElement('input');
         labelInput.type = 'text';
         labelInput.className = 'screenshotLabel';
-        labelInput.value = screenshotData.label +
-                           (Object.keys(screenshotData.values).length > 0 ?
-                           ': ' + JSON.stringify(screenshotData.values) : '');
+        labelInput.value = createHumanReadableLabel(screenshotData);
         screenshotDiv.appendChild(labelInput);
 
         screenshotsContainer.appendChild(screenshotDiv);
     });
+}
+
+function createHumanReadableLabel(screenshotData) {
+    if (screenshotData.label === 'CLICK') {
+        return `click at position (${screenshotData.values.x}, ${screenshotData.values.y})`;
+    } else if (screenshotData.label === 'KEY_INPUT' || screenshotData.values.endKeyInput === true) {
+        var label = 'input text "';
+        let lastChar = '"';
+        for (var i = 0; i < screenshotData.values.fullKeyInputSequence.length; i++) {
+            var key = screenshotData.values.fullKeyInputSequence[i];
+            if (key === 'Enter' && i === screenshotData.values.fullKeyInputSequence.length - 1) {
+                lastChar = '';
+                label += '" and press Enter';
+                break;
+            } else {
+                label += key;
+            }
+        }
+        label += lastChar;
+        return label;
+    }
+    return screenshotData.label;
 }
 
 document.addEventListener('DOMContentLoaded', function () {
