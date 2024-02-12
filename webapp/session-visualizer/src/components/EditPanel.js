61c55478c54b16c3854615d5db2cfdafc591d406
George Luo <gluo@Georges-MacBook-Air.local>
2024-02-11 19:53:59 -0800

    edit panel styling

--- webapp/session-visualizer/src/components/EditPanel.js
+++ webapp/session-visualizer/src/components/EditPanel.js
@@ -1,39 +1,41 @@
 import React, { useEffect, useState } from "react";
+import "./EditPanel.css"; // Importing the CSS file for styles
 
 const EditPanel = ({ selectedObject, onSelectedObjectChange }) => {
-    const [editFields, setEditFields] = useState(selectedObject);
+  const [editFields, setEditFields] = useState(selectedObject);
 
-    useEffect(() => {
-        setEditFields(selectedObject);
-    }, [selectedObject]);
+  useEffect(() => {
+    setEditFields(selectedObject);
+  }, [selectedObject]);
 
-    const handleInputChange = (key, value) => {
-        const updatedFields = { ...editFields, [key]: value };
-        setEditFields(updatedFields);
-    };
+  const handleInputChange = (key, value) => {
+    const updatedFields = { ...editFields, [key]: value };
+    setEditFields(updatedFields);
+  };
 
-    useEffect(() => {
-        onSelectedObjectChange(editFields);
-    }, [editFields]);
+  useEffect(() => {
+    onSelectedObjectChange(editFields);
+  }, [editFields]);
 
-    const renderEditFields = (object) => {
-        return Object.entries(object).map(([key, value]) => (
-            <div key={key} className="edit-field">
-                <label>{key}</label>
-                <input
-                    type="text"
-                    value={value}
-                    onChange={(e) => handleInputChange(key, e.target.value)}
-                />
-            </div>
-        ));
-    };
+  const renderEditFields = (object) => {
+    return Object.entries(object).map(([key, value]) => (
+      <div key={key} className="edit-field">
+        <label className="edit-label">{key}</label> {/* Applying class for the label */}
+        <input
+          type="text"
+          value={value}
+          onChange={(e) => handleInputChange(key, e.target.value)}
+          className="edit-input" // Applying class for the input
+        />
+      </div>
+    ));
+  };
 
-    return (
-        <div className="edit-panel">
-            {editFields && renderEditFields(editFields)}
-        </div>
-    );
+  return (
+    <div className="edit-panel">
+      {editFields && renderEditFields(editFields)}
+    </div>
+  );
 };
 
 export default EditPanel;
--- webapp/session-visualizer/src/components/Editor.js
+++ webapp/session-visualizer/src/components/Editor.js
@@ -1,43 +1,7 @@
 import React, { useEffect, useState } from "react";
 import "./Editor.css";
 import ObjectViewer from "./ObjectViewer.js";
-
-const EditPanel = ({ selectedObject, onSelectedObjectChange }) => {
-    const [editFields, setEditFields] = useState(selectedObject);
-
-    useEffect(() => {
-        setEditFields(selectedObject);
-    }, [selectedObject]);
-
-    const handleInputChange = (key, value) => {
-        const updatedFields = { ...editFields, [key]: value };
-        setEditFields(updatedFields);
-    };
-
-    useEffect(() => {
-        onSelectedObjectChange(editFields);
-    }, [editFields]);
-
-    const renderEditFields = (object) => {
-        return Object.entries(object).map(([key, value]) => (
-            <div key={key} className="edit-field">
-                <label>{key}</label>
-                <input
-                    type="text"
-                    value={value}
-                    onChange={(e) => handleInputChange(key, e.target.value)}
-                />
-            </div>
-        ));
-    };
-
-    return (
-        <div className="edit-panel">
-            {editFields && renderEditFields(editFields)}
-        </div>
-    );
-};
-
+import EditPanel from "./EditPanel.js";
 const Editor = ({
     onClose,
     selectedIndex,
